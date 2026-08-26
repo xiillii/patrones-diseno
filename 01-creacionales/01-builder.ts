3769a4c826313e5b80f884fde8488b65cddf8ed6
Jose Alonso <mail@josealonso.dev>
2026-08-26 11:18:19 -0600

    se agregan el builder de Computer

--- 01-creacionales/01-builder.ts
+++ 01-creacionales/01-builder.ts
@@ -12,3 +12,55 @@
  *
  * https://refactoring.guru/es/design-patterns/builder
  */
+
+class Computer {
+  public cpu: string = "cpu - not defined";
+  public ram: string = "ram - not defined";
+  public storage: string = "storage - not defined";
+  public gpu?: string;
+
+  displayConfiguration() {
+    console.log(`Configuración de la computadora
+        CPU: ${this.cpu}
+        RAM: ${this.ram}
+        Almacenamiento: ${this.storage}
+        GPU: ${this.gpu}
+        `);
+  }
+}
+
+class ComputerBuilder {
+  private _computer: Computer;
+
+  constructor() {
+    this._computer = new Computer();
+  }
+
+  setCPU(cpu: string): ComputerBuilder {
+    this._computer.cpu = cpu;
+
+    return this;
+  }
+
+  setRAM(ram: string): ComputerBuilder {
+    this._computer.ram = ram;
+
+    return this;
+  }
+
+  setStorage(storage: string): ComputerBuilder {
+    this._computer.storage = storage;
+
+    return this;
+  }
+
+  setGPU(gpu: string): ComputerBuilder {
+    this._computer.gpu = gpu;
+
+    return this;
+  }
+
+  build() {
+    return this._computer;
+  }
+}
