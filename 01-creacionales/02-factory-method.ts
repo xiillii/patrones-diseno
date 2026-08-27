8c7f7eed8261d57c6e1dbf56bff29920ddefc483
Jose Alonso <mail@josealonso.dev>
2026-08-26 18:14:03 -0600

    factory, inicial

--- 01-creacionales/02-factory-method.ts
+++ 01-creacionales/02-factory-method.ts
@@ -12,3 +12,33 @@
  * https://refactoring.guru/es/design-patterns/factory-method
  *
  */
+
+import { COLORS } from "../helpers/colors.ts";
+
+interface Hamburger {
+  prepare(): void;
+}
+
+class ChickedHamburger implements Hamburger {
+  prepare(): void {
+    console.log("Preparando una hamburgesa de %cpollo", COLORS.yellow);
+  }
+}
+
+class BeefHamburger implements Hamburger {
+  prepare(): void {
+    console.log("Preparando una hamburgesa de %cres", COLORS.brown);
+  }
+}
+
+function main() {
+  let hP: Hamburger = new ChickedHamburger();
+
+  hP.prepare();
+
+  hP = new BeefHamburger();
+
+  hP.prepare();
+}
+
+main();
