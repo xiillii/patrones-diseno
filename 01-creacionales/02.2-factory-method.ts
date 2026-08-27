bad1e62442a15e29507c34e0c3989b95e67d809d
Jose Alonso <mail@josealonso.dev>
2026-08-27 15:18:48 -0600

    agregando configuracion auto-formateado

--- 01-creacionales/02.2-factory-method.ts
+++ 01-creacionales/02.2-factory-method.ts
@@ -14,17 +14,17 @@
 
 /**
  * 	!Descripción:
-  1.	Completen las clases SalesReport e InventoryReport para implementar 
+  1.	Completen las clases SalesReport e InventoryReport para implementar
       la interfaz Report, generando el contenido de cada reporte en el método generate.
-	  
-  2.	Implementen las clases SalesReportFactory e InventoryReportFactory 
+
+  2.	Implementen las clases SalesReportFactory e InventoryReportFactory
       para crear instancias de SalesReport y InventoryReport, respectivamente.
 
-	3.	Prueben el programa generando diferentes tipos de reportes usando
+  3.	Prueben el programa generando diferentes tipos de reportes usando
       el prompt para seleccionar el tipo de reporte.
  */
 
-import { COLORS } from '../helpers/colors.ts';
+import { COLORS } from "../helpers/colors.ts";
 
 // 1. Definir la interfaz Report
 interface Report {
@@ -37,6 +37,10 @@ interface Report {
 class SalesReport implements Report {
   // TODO: implementar el método e imprimir en consola:
   // 'Generando reporte de ventas...'
+
+  generate(): void {
+    console.log("");
+  }
 }
 
 class InventoryReport implements Report {
@@ -59,13 +63,13 @@ abstract class ReportFactory {
 
 class SalesReportFactory extends ReportFactory {
   createReport(): Report {
-    throw new Error('Method not implemented.');
+    throw new Error("Method not implemented.");
   }
 }
 
 class InventoryReportFactory extends ReportFactory {
   createReport(): Report {
-    throw new Error('Method not implemented.');
+    throw new Error("Method not implemented.");
   }
 }
 
@@ -75,11 +79,11 @@ function main() {
   let reportFactory: ReportFactory;
 
   const reportType = prompt(
-    '¿Qué tipo de reporte deseas? %c(sales/inventory)',
-    COLORS.red
+    "¿Qué tipo de reporte deseas? %c(sales/inventory)",
+    COLORS.red,
   );
 
-  if (reportType === 'sales') {
+  if (reportType === "sales") {
     reportFactory = new SalesReportFactory();
   } else {
     reportFactory = new InventoryReportFactory();
