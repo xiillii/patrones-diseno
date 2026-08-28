e21882b929425acf15c12d5bfd96803026ff4bee
Jose Alonso <mail@josealonso.dev>
2026-08-28 00:00:26 -0600

    factory functions

--- 01-creacionales/07-factory-function.ts
+++ 01-creacionales/07-factory-function.ts
@@ -5,5 +5,33 @@
  *
  * * Es útil cuando necesitamos crear objetos o funciones de manera dinámica,
  * * es decir, en tiempo de ejecución y no en tiempo de compilación.
- *
  */
+
+import { COLORS } from '../helpers/colors.ts';
+
+type Language = 'es' | 'en' | 'fr';
+
+//i18n
+function createGreeter(lang: Language) {
+  return function (name: string) {
+    const messages = {
+      es: `Hola, %c${name}`,
+      en: `Hello, %c${name}`,
+      fr: `Bonjour, %c${name}`,
+    };
+
+    return console.log(messages[lang], COLORS.red);
+  };
+}
+
+function main(): void {
+  const spanishGreeter = createGreeter('es');
+  const englishGreeter = createGreeter('en');
+  const frenchGreeter = createGreeter('fr');
+
+  spanishGreeter('Oncho');
+  englishGreeter('Sam');
+  frenchGreeter('Nat');
+}
+
+main();
