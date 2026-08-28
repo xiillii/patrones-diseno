68420961347a800098d0b5759de6ebe88092141e
Jose Alonso <mail@josealonso.dev>
2026-08-27 20:56:55 -0600

    inmutabilidad con copia

--- 01-creacionales/05-inmutabilidad.ts
+++ 01-creacionales/05-inmutabilidad.ts
@@ -4,5 +4,64 @@
  * En estos casos, se puede hacer una copia del objeto y modificar la copia.
  *
  *  * Es útil para mantener un historial de estados en aplicaciones interactivas.
- *
  */
+
+import { COLORS } from '../helpers/colors.ts';
+
+class CodeEditorState {
+  readonly content: string;
+  readonly cursorPosition: number;
+  readonly unsavedChanges: boolean;
+
+  constructor(
+    content: string,
+    cursorPosition: number,
+    unsavedChanges: boolean,
+  ) {
+    this.content = content;
+    this.cursorPosition = cursorPosition;
+    this.unsavedChanges = unsavedChanges;
+  }
+
+  copyWith(
+    { content, cursorPosition, unsavedChanges }: Partial<CodeEditorState>,
+  ): CodeEditorState {
+    return new CodeEditorState(
+      content ?? this.content,
+      cursorPosition ?? this.cursorPosition,
+      unsavedChanges ?? this.unsavedChanges,
+    );
+  }
+
+  displayState() {
+    console.log('\n%cEstado del editor:', COLORS.green);
+    console.log(`
+        Content: ${this.content}
+        Cursor position: ${this.cursorPosition}
+        Unsaved changes: ${this.unsavedChanges}
+        `);
+  }
+}
+
+class CodeEditorHistory {
+  private history: CodeEditorState[] = [];
+  private currentIndex: number = -1;
+
+  save(state: CodeEditorState): void {
+    if (this.currentIndex < this.history.length - 1) {
+      this.history = this.history.splice(0, this.currentIndex + 1);
+    }
+
+    this.history.push(state);
+    this.currentIndex++;
+  }
+
+  redo(): CodeEditorState | null {
+    if (this.currentIndex < this.history.length - 1) {
+      this.currentIndex++;
+      return this.history[this.currentIndex];
+    }
+
+    return null;
+  }
+}
