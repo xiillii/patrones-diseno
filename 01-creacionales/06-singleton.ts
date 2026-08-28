2fc0e15e9ba28f134c135c21f650cf7fb7062b0f
Jose Alonso <mail@josealonso.dev>
2026-08-27 22:18:00 -0600

    creando singleton

--- 01-creacionales/06-singleton.ts
+++ 01-creacionales/06-singleton.ts
@@ -9,3 +9,53 @@
  *
  * https://refactoring.guru/es/design-patterns/singleton
  */
+
+import { COLORS } from '../helpers/colors.ts';
+
+class DragonBalls {
+  private static instance: DragonBalls;
+  private ballsCollected: number;
+
+  private constructor() {
+    this.ballsCollected = 0;
+  }
+
+  public static getInstance(): DragonBalls {
+    if (!this.instance) {
+      this.instance = new DragonBalls();
+      console.log('%cLas pelotas del Dragón han sido creadas!', COLORS.orange);
+    }
+
+    return this.instance;
+  }
+
+  collectBall(): void {
+    if (this.ballsCollected >= 7) {
+      console.log(
+        '%cYa se han recolectado las 7 esferas del Dragón! Invoca a Shenlong',
+        COLORS.violet,
+      );
+      return;
+    }
+    this.ballsCollected++;
+    console.log(
+      `%cEsfera del Dragón recolectada.\n%cTotal de esferas: %c${this.ballsCollected}`,
+      COLORS.red,
+      COLORS.cyan,
+      COLORS.pink,
+    );
+  }
+
+  summonShenlong() {
+    if (this.ballsCollected === 7) {
+      console.log('%cShenlong ha sido invocado, pide tu deseo!', COLORS.pink);
+      this.ballsCollected = 0;
+      return;
+    }
+
+    console.log(
+      `%cAún faltan ${7 - this.ballsCollected} esferas para invocar a Shenlong`,
+      COLORS.red,
+    );
+  }
+}
