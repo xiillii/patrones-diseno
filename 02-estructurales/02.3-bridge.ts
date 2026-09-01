77aab94e7eaa8c6f8cbaea580d31a911fc68250f
Jose Alonso <mail@josealonso.dev>
2026-09-01 11:03:38 -0600

    bridge, otro ejemplo

--- 02-estructurales/02.3-bridge.ts
+++ 02-estructurales/02.3-bridge.ts
@@ -9,3 +9,71 @@
  *
  * https://refactoring.guru/es/design-patterns/bridge
  */
+
+import { COLORS } from "../helpers/colors.ts";
+
+// 1. Interfaz NotificationChannel
+// Define el método `send`, que cada canal de comunicación implementará.
+interface NotificationChannel {
+  send(message: string): void;
+}
+
+// 2. Implementaciones de Canales de Comunicación
+
+class EmailChannel implements NotificationChannel {
+  send(message: string): void {
+    console.log(`Enviando correo electrónico: ${message}`);
+  }
+}
+
+class SMSChannel implements NotificationChannel {
+  send(message: string): void {
+    console.log(`Enviando SMS: ${message}`);
+  }
+}
+
+class PushNotificationChannel implements NotificationChannel {
+  send(message: string): void {
+    console.log(`Enviando Push: ${message}`);
+  }
+}
+
+// 3. Clase Abstracta Notification
+// Define la propiedad `channel` y el método `notify`
+
+abstract class Notification {
+  protected channels: NotificationChannel[];
+
+  constructor(channels: NotificationChannel[]) {
+    this.channels = channels;
+  }
+  abstract notify(message: string): void;
+  abstract addChannel(channel: NotificationChannel): void;
+  abstract removeLastChannel(): void;
+}
+
+class AlertNotification extends Notification {
+  override notify(message: string): void {
+    console.log("\n%cNotificación de alerta", COLORS.red);
+    this.channels.forEach((channel) => channel.send(message));
+  }
+  override addChannel(channel: NotificationChannel): void {
+    this.channels.push(channel);
+  }
+
+  override removeLastChannel(): void {
+    this.channels.pop();
+  }
+}
+
+function main(): void {
+  const alerts = new AlertNotification([new EmailChannel(), new SMSChannel()]);
+
+  alerts.notify("No paso su tarjeta");
+  alerts.removeLastChannel();
+  alerts.addChannel(new PushNotificationChannel());
+
+  alerts.notify("Tiene que actualizar su tarjeta");
+}
+
+main();
