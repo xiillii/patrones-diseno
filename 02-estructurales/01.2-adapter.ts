e26248cd2d948bc4bb79cff10ffe25c2f577734a
Jose Alonso <mail@josealonso.dev>
2026-08-31 23:07:05 -0600

    tarea adapter

--- 02-estructurales/01.2-adapter.ts
+++ 02-estructurales/01.2-adapter.ts
@@ -11,7 +11,7 @@
  * https://refactoring.guru/es/design-patterns/adapter
  */
 
-import { COLORS } from '../helpers/colors.ts';
+import { COLORS } from "../helpers/colors.ts";
 
 // 1. Interfaz PaymentProcessor
 interface PaymentProcessor {
@@ -37,7 +37,7 @@ class MercadoPagoService {
   pay(amount: number): void {
     console.log(
       `Procesando pago de $${amount} con %cMercadoPago`,
-      COLORS.yellow
+      COLORS.yellow,
     );
   }
 }
@@ -45,18 +45,30 @@ class MercadoPagoService {
 // 3. Clases Adaptadoras
 
 // Adaptador para PayPal
-class PayPalAdapter {
-  // TODO: Implementar la interfaz PaymentProcessor
+class PayPalAdapter implements PaymentProcessor {
+  private payService = new PayPalService();
+
+  processPayment(amount: number): void {
+    this.payService.sendPayment(amount);
+  }
 }
 
 // Adaptador para Stripe
-class StripeAdapter {
-  // TODO: Implementar la interfaz PaymentProcessor
+class StripeAdapter implements PaymentProcessor {
+  private payService = new StripeService();
+
+  processPayment(amount: number): void {
+    this.payService.makeCharge(amount);
+  }
 }
 
 // Adaptador para MercadoPago
-class MercadoPagoAdapter {
-  // TODO: Implementar la interfaz PaymentProcessor
+class MercadoPagoAdapter implements PaymentProcessor {
+  private payService = new MercadoPagoService();
+
+  processPayment(amount: number): void {
+    this.payService.pay(amount);
+  }
 }
 
 // 4. Código Cliente para probar el Adapter
@@ -71,13 +83,13 @@ function main() {
 
   // Procesar pagos con los diferentes servicios
   // Los 3 procesadores de pago trabajan exactamente igual después de adaptaros
-  console.log('Usando PayPal:');
+  console.log("Usando PayPal:");
   paypalProcessor.processPayment(paymentAmount);
 
-  console.log('\nUsando Stripe:');
+  console.log("\nUsando Stripe:");
   stripeProcessor.processPayment(paymentAmount);
 
-  console.log('\nUsando MercadoPago:');
+  console.log("\nUsando MercadoPago:");
   mercadoPagoProcessor.processPayment(paymentAmount);
 }
 
