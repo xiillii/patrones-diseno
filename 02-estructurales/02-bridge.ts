4f59e2d3ddf44c1f844fa46858e5dfa1595cd474
Jose Alonso <mail@josealonso.dev>
2026-09-01 09:47:03 -0600

    Brigde inicial

--- 02-estructurales/02-bridge.ts
+++ 02-estructurales/02-bridge.ts
@@ -9,3 +9,57 @@
  *
  * https://refactoring.guru/es/design-patterns/bridge
  */
+
+import { COLORS } from "../helpers/colors.ts";
+
+interface Ability {
+  use(): void;
+}
+
+class SwordAttack implements Ability {
+  use(): void {
+    console.log("Ataca con una %cespada ferozmente", COLORS.blue);
+  }
+}
+
+class MagicSpell implements Ability {
+  use(): void {
+    console.log("Lanza un hechizo %cmágico poderoso", COLORS.green);
+  }
+}
+
+abstract class Character {
+  protected ability: Ability;
+
+  constructor(ability: Ability) {
+    this.ability = ability;
+  }
+
+  setAbility(ability: Ability): void {
+    this.ability = ability;
+  }
+
+  abstract performAbility(): void;
+}
+
+class Warrior extends Character {
+  override performAbility(): void {
+    console.log("\nEl guerrero está listo para luchar");
+    this.ability.use();
+  }
+}
+
+class Mage extends Character {
+  override performAbility(): void {
+    console.log("\nEl mago está listo para luchar");
+    this.ability.use();
+  }
+}
+
+function main(): void {
+  const warrior = new Warrior(new SwordAttack());
+
+  warrior.performAbility();
+}
+
+main();
