d991b31ef791b138c5195c401dc4e0a71957ce63
Jose Alonso <mail@josealonso.dev>
2026-09-02 15:13:58 -0600

    creando el iterator

--- 03-comportamiento/03-iterator.ts
+++ 03-comportamiento/03-iterator.ts
@@ -8,3 +8,66 @@
  *
  * https://refactoring.guru/es/design-patterns/iterator
  */
+
+interface Iterator<T> {
+  next(): T | null;
+  hasNext(): boolean;
+  current(): T | null;
+}
+
+class Pokemon {
+  public name: string;
+  public type: string;
+
+  constructor(name: string, type: string) {
+    this.name = name;
+    this.type = type;
+  }
+}
+
+class PokemonCollection {
+  private pokemons: Pokemon[] = [];
+
+  addPokemon(pokemon: Pokemon) {
+    this.pokemons.push(pokemon);
+  }
+
+  getPokemonAt(index: number): Pokemon | null {
+    if (index >= 0 && index < this.pokemons.length) {
+      return this.pokemons[index];
+    }
+
+    return null;
+  }
+
+  getLength(): number {
+    return this.pokemons.length;
+  }
+
+  createIterator(): PokemonIterator {
+    return new PokemonIterator(this);
+  }
+}
+
+class PokemonIterator implements Iterator<Pokemon> {
+  private collection: PokemonCollection;
+  private position: number = 0;
+
+  constructor(collection: PokemonCollection) {
+    this.collection = collection;
+  }
+
+  next(): Pokemon | null {
+    if (this.hasNext()) {
+      return this.collection.getPokemonAt(++this.position);
+    }
+
+    return null;
+  }
+  hasNext(): boolean {
+    return this.position < this.collection.getLength();
+  }
+  current(): Pokemon | null {
+    return this.collection.getPokemonAt(this.position);
+  }
+}
