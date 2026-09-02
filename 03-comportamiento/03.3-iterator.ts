ced421a8e16c18c9f844c9326e443a040b822a85
Jose Alonso <mail@josealonso.dev>
2026-09-02 15:29:52 -0600

    tarea iterator

--- 03-comportamiento/03.3-iterator.ts
+++ 03-comportamiento/03.3-iterator.ts
@@ -29,10 +29,16 @@ class CardCollection {
   }
 
   //TODO: Implementación del iterador usando Symbol.iterator
-  // Symbol.iterator (): IterableIterator<Card>
+  *[Symbol.iterator](): IterableIterator<Card> {
+    yield* this.cards;
+  }
 
   // TODO: Implementación del iterador usando Generadores
-  // *getCard(): IterableIterator<Card>
+  *getCard(): IterableIterator<Card> {
+    for (const card of this.cards) {
+      yield card;
+    }
+  }
 }
 
 // Código Cliente para probar el iterador
