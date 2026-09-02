be7d521e795550f04839eba0baefa97d050d7205
Jose Alonso <mail@josealonso.dev>
2026-09-02 10:25:42 -0600

    creando ejercicio chan responsabilitiy

--- 03-comportamiento/01-chain-responsibility.ts
+++ 03-comportamiento/01-chain-responsibility.ts
@@ -9,3 +9,73 @@
  *
  * https://refactoring.guru/es/design-patterns/chain-of-responsibility
  */
+
+import { COLORS } from '../helpers/colors.ts';
+
+interface Handler {
+  setNext(handler: Handler): Handler;
+  handle(request: string): void;
+}
+
+abstract class BaseHandler implements Handler {
+  private nextHandler?: Handler;
+
+  setNext(handler: Handler): Handler {
+    this.nextHandler = handler;
+
+    return handler;
+  }
+
+  handle(request: string): void {
+    if (this.nextHandler) {
+      this.nextHandler.handle(request);
+    }
+  }
+}
+
+// Soporte básico
+class BasicSupport extends BaseHandler {
+  override handle(request: string): void {
+    if (request === 'básico') {
+      console.log(
+        '%cSoporte básico: Resolviendo problema básico',
+        COLORS.green,
+      );
+      return;
+    }
+
+    console.log('Soporte básico: Pasando el problema a soporte avanzado');
+    super.handle(request);
+  }
+}
+
+// Soporte avanzado
+class AdvancedSupport extends BaseHandler {
+  override handle(request: string): void {
+    if (request === 'avanzado') {
+      console.log(
+        '%cSoporte avanzado: Resolviendo problema avanzado',
+        COLORS.green,
+      );
+      return;
+    }
+
+    console.log('Soporte avanzado: Pasando el problema a soporte experto');
+    super.handle(request);
+  }
+}
+
+// Soporte experto
+class ExpertSupport extends BaseHandler {
+  override handle(request: string): void {
+    if (request === 'experto') {
+      console.log(
+        '%cSoporte experto: Resolviendo problema experto',
+        COLORS.green,
+      );
+      return;
+    }
+
+    console.log('Soporte experto: No hay nada que hacer');
+  }
+}
