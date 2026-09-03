e83e76573d314767bdef0118179ae5416d04774b
Jose Alonso <mail@josealonso.dev>
2026-09-02 23:43:53 -0600

    patron mediator

--- 03-comportamiento/04-mediator.ts
+++ 03-comportamiento/04-mediator.ts
@@ -9,3 +9,53 @@
  *
  * https://refactoring.guru/es/design-patterns/mediator
  */
+
+import { COLORS } from '../helpers/colors.ts';
+
+// chatroom
+class ChatRoom {
+  private users: User[] = [];
+  private title: string;
+
+  constructor(title: string) {
+    this.title = title;
+  }
+
+  addUser(user: User) {
+    this.users.push(user);
+  }
+
+  sendMessage(sender: User, message: string): void {
+    for (const user of this.users.filter((u) => u !== sender)) {
+      user.receiveMessage(sender, message);
+    }
+  }
+}
+
+class User {
+  private username: string;
+  private chatRoom: ChatRoom;
+
+  constructor(username: string, chatRoom: ChatRoom) {
+    this.username = username;
+    this.chatRoom = chatRoom;
+  }
+
+  sendMessage(message: string): void {
+    console.log(
+      `\n\n%c${this.username} envía: %c${message}`,
+      COLORS.violet,
+      COLORS.purple,
+    );
+    this.chatRoom.sendMessage(this, message);
+  }
+
+  receiveMessage(sender: User, message: string): void {
+    console.log(
+      `\n\n%c${this.username} recibe de %c${sender.username}: %c${message}`,
+      COLORS.blue,
+      COLORS.pink,
+      COLORS.orange,
+    );
+  }
+}
