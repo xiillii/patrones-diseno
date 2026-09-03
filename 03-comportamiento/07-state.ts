8fadcaa71b62b61cf703cc3202e282cc6e854451
Jose Alonso <mail@josealonso.dev>
2026-09-03 02:45:35 -0600

    ejercicio State

--- 03-comportamiento/07-state.ts
+++ 03-comportamiento/07-state.ts
@@ -9,6 +9,9 @@
  * https://refactoring.guru/es/design-patterns/state
  */
 
+import { COLORS } from '../helpers/colors.ts';
+import { sleep } from '../helpers/sleep.ts';
+
 /**
  * * Objetivo: Implementar el patrón State para simular el funcionamiento
  * * de una máquina expendedora.
@@ -18,3 +21,160 @@
  *  * Entregando Producto,
  * * y su comportamiento varía dependiendo del estado actual.
  */
+
+interface State {
+  name: string;
+
+  insertMoney(): void;
+  selectProduct(): void;
+  dispenseProduct(): void;
+}
+
+class VendingMachine {
+  private state: State;
+
+  constructor() {
+    this.state = new WaitingForMoney(this);
+  }
+
+  insertMoney(): void {
+    this.state.insertMoney();
+  }
+  selectProduct(): void {
+    this.state.selectProduct();
+  }
+  dispenseProduct(): void {
+    this.state.dispenseProduct();
+  }
+
+  setState(newState: State) {
+    this.state = newState;
+
+    console.log(`El estado cambió a %c${newState.name}`, COLORS.blue);
+  }
+
+  getStateName(): string {
+    return this.state.name;
+  }
+}
+
+class WaitingForMoney implements State {
+  public name: string = 'Esperando dinero';
+  private vendingMachine: VendingMachine;
+
+  constructor(vendingMachine: VendingMachine) {
+    this.vendingMachine = vendingMachine;
+  }
+
+  insertMoney(): void {
+    console.log(
+      'Dinero insertado: %cAhora puedes seleccionar un producto',
+      COLORS.green,
+    );
+    this.vendingMachine.setState(new ProductSelected(this.vendingMachine));
+  }
+  selectProduct(): void {
+    console.log('%cPrimero debes de insertar dinero.', COLORS.red);
+  }
+  dispenseProduct(): void {
+    console.log('%cPrimero debes de insertar dinero.', COLORS.red);
+  }
+}
+
+class ProductSelected implements State {
+  public name: string = 'Seleccionando producto';
+  private vendingMachine: VendingMachine;
+
+  constructor(vendingMachine: VendingMachine) {
+    this.vendingMachine = vendingMachine;
+  }
+
+  insertMoney(): void {
+    console.log(
+      '%cPor favor selecciona un producto - dinero ya insertado',
+      COLORS.red,
+    );
+  }
+  selectProduct(): void {
+    console.log('%cProducto seleccionado.', COLORS.red);
+    this.vendingMachine.setState(new DispensingProduct(this.vendingMachine));
+  }
+  dispenseProduct(): void {
+    console.log(
+      '%cPor favor selecciona un producto - antes de despacharlo.',
+      COLORS.red,
+    );
+  }
+}
+
+class DispensingProduct implements State {
+  public name: string = 'Despachando producto';
+  private vendingMachine: VendingMachine;
+
+  constructor(vendingMachine: VendingMachine) {
+    this.vendingMachine = vendingMachine;
+  }
+
+  insertMoney(): void {
+    console.log(
+      '%cPor favor espera a que se entregue el producto',
+      COLORS.red,
+    );
+  }
+  selectProduct(): void {
+    console.log('%cProducto ya seleccionado y despachando', COLORS.red);
+  }
+  dispenseProduct(): void {
+    console.log(
+      '%cProducto despachado, cambiando estado a EsperandoDinero.',
+      COLORS.green,
+    );
+    this.vendingMachine.setState(new WaitingForMoney(this.vendingMachine));
+  }
+}
+
+async function main() {
+  const vendingMachine = new VendingMachine();
+
+  let selectedOption: string | null = '4';
+
+  do {
+    console.clear();
+    console.log(
+      `Selecciona una opción: %c${vendingMachine.getStateName()}`,
+      COLORS.blue,
+    );
+    selectedOption = prompt(
+      `
+        1. Insertar dinero
+        2. Seleccionar producto
+        3. Despachar producto
+        4. Salir
+
+        opción:
+      `,
+    );
+
+    switch (selectedOption) {
+      case '1':
+        vendingMachine.insertMoney();
+        break;
+      case '2':
+        vendingMachine.selectProduct();
+        break;
+      case '3':
+        vendingMachine.dispenseProduct();
+        break;
+      case '4':
+        console.log('Saliendo del sistema');
+        break;
+      default:
+        break;
+    }
+
+    await sleep(3000);
+  } while (selectedOption !== '4');
+}
+
+main();
+ 
