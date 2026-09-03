d936b993ee84c150313af9bdab7f5ec57444ce39
Jose Alonso <mail@josealonso.dev>
2026-09-03 12:51:12 -0600

    ejercicio visitor

--- 03-comportamiento/10-visitor.ts
+++ 03-comportamiento/10-visitor.ts
@@ -11,6 +11,8 @@
  * https://refactoring.guru/es/design-patterns/visitor
  */
 
+import { COLORS } from '../helpers/colors.ts';
+
 /**
  * Contexto: Imagina que estás diseñando un sistema para un parque
  * temático con diferentes tipos de atracciones:
@@ -24,3 +26,151 @@
  * de la atracción como del tipo de visitante,
  * sin modificar las clases originales.
  */
+
+interface Visitor {
+  visitRollerCoaster(rollerCoaster: RollerCoaster): void;
+  visitHauntedHouse(hauntedHouse: HauntedHouse): void;
+  visitFerrisWheel(ferrisWheel: FerrisWheel): void;
+}
+
+interface Attraction {
+  accept(visitor: Visitor): void;
+}
+
+class RollerCoaster implements Attraction {
+  private price: number = 50;
+
+  getPrice(): number {
+    return this.price;
+  }
+
+  accept(visitor: Visitor): void {
+    visitor.visitRollerCoaster(this);
+  }
+}
+
+class HauntedHouse implements Attraction {
+  private price: number = 40;
+
+  getPrice(): number {
+    return this.price;
+  }
+
+  accept(visitor: Visitor): void {
+    visitor.visitHauntedHouse(this);
+  }
+}
+
+class FerrisWheel implements Attraction {
+  private price: number = 30;
+
+  getPrice(): number {
+    return this.price;
+  }
+
+  accept(visitor: Visitor): void {
+    visitor.visitFerrisWheel(this);
+  }
+}
+
+// Visitors
+class ChildVisitor implements Visitor {
+  visitRollerCoaster(rollerCoaster: RollerCoaster): void {
+    console.log(
+      `%cNiño en Montaña Rusa: Precio con descuento de ${
+        rollerCoaster.getPrice() * 0.5
+      }`,
+      COLORS.brown,
+    );
+  }
+  visitHauntedHouse(hauntedHouse: HauntedHouse): void {
+    console.log(
+      `%cNiño en Casa Embrujada: Precio con descuento de ${
+        hauntedHouse.getPrice() * 0.7
+      }`,
+      COLORS.brown,
+    );
+  }
+  visitFerrisWheel(ferrisWheel: FerrisWheel): void {
+    console.log(
+      `%cNiño en Rueda de la Fortuna: Precio con descuento de ${
+        ferrisWheel.getPrice() * 0.6
+      }`,
+      COLORS.brown,
+    );
+  }
+}
+
+class AdultVisitor implements Visitor {
+  visitRollerCoaster(rollerCoaster: RollerCoaster): void {
+    console.log(
+      `%cAdulto en Montaña Rusa: Precio con descuento de ${rollerCoaster.getPrice()}`,
+      COLORS.cyan,
+    );
+  }
+  visitHauntedHouse(hauntedHouse: HauntedHouse): void {
+    console.log(
+      `%cAdulto en Casa Embrujada: Precio con descuento de ${hauntedHouse.getPrice()}`,
+      COLORS.cyan,
+    );
+  }
+  visitFerrisWheel(ferrisWheel: FerrisWheel): void {
+    console.log(
+      `%cAdulto en Rueda de la Fortuna: Precio con descuento de ${ferrisWheel.getPrice()}`,
+      COLORS.cyan,
+    );
+  }
+}
+
+class SeniorVisitor implements Visitor {
+  visitRollerCoaster(rollerCoaster: RollerCoaster): void {
+    console.log(
+      `%cAdulto en Montaña Rusa: Precio con descuento de ${
+        rollerCoaster.getPrice() * 85
+      }`,
+      COLORS.pink,
+    );
+  }
+  visitHauntedHouse(hauntedHouse: HauntedHouse): void {
+    console.log(
+      `%cAdulto en Casa Embrujada: Precio con descuento de ${
+        hauntedHouse.getPrice() * 85
+      }`,
+      COLORS.pink,
+    );
+  }
+  visitFerrisWheel(ferrisWheel: FerrisWheel): void {
+    console.log(
+      `%cAdulto en Rueda de la Fortuna: Precio con descuento de ${
+        ferrisWheel.getPrice() * 85
+      }`,
+      COLORS.pink,
+    );
+  }
+}
+
+function main() {
+  const rollerCoaster = new RollerCoaster();
+  const hauntedHouse = new HauntedHouse();
+  const ferrisWheel = new FerrisWheel();
+
+  const child = new ChildVisitor();
+
+  child.visitRollerCoaster(rollerCoaster);
+  child.visitHauntedHouse(hauntedHouse);
+  child.visitFerrisWheel(ferrisWheel);
+
+  const adult = new AdultVisitor();
+
+  adult.visitRollerCoaster(rollerCoaster);
+  adult.visitHauntedHouse(hauntedHouse);
+  adult.visitFerrisWheel(ferrisWheel);
+
+  const senior = new SeniorVisitor();
+
+  senior.visitRollerCoaster(rollerCoaster);
+  senior.visitHauntedHouse(hauntedHouse);
+  senior.visitFerrisWheel(ferrisWheel);
+}
+
+main();
